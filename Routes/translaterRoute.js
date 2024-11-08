fb504e5d88183ed7dc87918276bcb5ace6cb6970
Muhammed <muhammedkerek2000@gmail.com>
2024-11-08 18:35:53 +0300

    Added the router to connect the folders together , making them work according to the MVC pattern

--- Routes/translaterRoute.js
+++ Routes/translaterRoute.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const translatorController = require("../controllers/translatorController");
+
+router.route("/").get(translatorController.renderHomePage);
+module.exports = router;
